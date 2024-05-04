4b7c2e682768206af09e50584505c0ca5f2bc6d4
edriso <edriso.dev@gmail.com>
2024-05-04 14:56:26 +0300

    update error message

--- src/helpers.php
+++ src/helpers.php
@@ -14,7 +14,7 @@ function base_path($path)
     return BASE_PATH . $path;
 }
 
-function abort($code = 404, $message = 'Resource not found.')
+function abort($code = 404, $message = 'Resource not found')
 {
     http_response_code($code);
 
