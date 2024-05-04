14c05a0ad10a68a2789ae67b73ab2a03d6d366dc
edriso <edriso.dev@gmail.com>
2024-05-05 00:36:43 +0300

    add font and favicon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-xl text-bold text-red-500">Hello, world!</h1>
+      <h1 className="text-xl text-bold text-blue-900">Hello, world!</h1>
     </>
   );
 }
