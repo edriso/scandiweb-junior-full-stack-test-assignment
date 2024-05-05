919d00ac591c5660ae6770cf9fcf1949b8f80df6
edriso <edriso.dev@gmail.com>
2024-05-05 09:28:12 +0300

    add Logo component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+import Logo from './Logo';
+
 function Navbar() {
   return (
-    <header className="">
-      <nav>navbar</nav>
+    <header className="flex items-center justify-between mb-14">
+      <nav>
+        <ul className="flex gap-6">
+          <li>Women</li>
+          <li>Men</li>
+          <li>Kids</li>
+        </ul>
+      </nav>
+
+      <Logo />
+
+      <div>cart</div>
     </header>
   );
 }
