dd3e38477466044b723ce08302d80be4782c3150
edriso <edriso.dev@gmail.com>
2024-05-05 12:34:48 +0300

    center logo in middle of screen

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,14 +4,17 @@ function Navbar() {
   return (
     <header className="flex items-center justify-between mb-14">
       <nav>
-        <ul className="flex gap-6">
-          <li>Women</li>
+        <ul className="flex gap-6 uppercase">
+          {/* active: text-accent border-accent font-semibold */}
+          <li className="border-b-2 border-transparent pb-4">Women</li>
           <li>Men</li>
           <li>Kids</li>
         </ul>
       </nav>
 
-      <Logo />
+      <div className="absolute inset-x-0 mx-auto flex items-center justify-center">
+        <Logo />
+      </div>
 
       <div>cart</div>
     </header>
