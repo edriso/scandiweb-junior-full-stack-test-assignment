1b8c86355f66ffc291ae3deb084aae5e7e55e2a3
edriso <edriso.dev@gmail.com>
2024-05-05 08:58:20 +0300

    add tailwind config of colors and font family

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      accent: '#5ECE7B',
+      text: '#1D1F22',
+    },
+    fontFamily: {
+      raleway: ['Raleway', 'system-ui', 'sans-serif'],
+      roboto: ['Roboto', 'system-ui', 'sans-serif'],
+    },
   },
   plugins: [],
 };
