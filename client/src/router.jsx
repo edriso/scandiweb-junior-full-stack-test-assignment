f945f5bafc2b3c7d6ed1ea1ff5a7f2d99e8a86e1
edriso <edriso.dev@gmail.com>
2024-05-07 05:28:29 +0300

    separate router into its own file

---
+++ client/src/router.jsx
@@ -0,0 +1,22 @@
+import { Categories, ErrorScreen, HomeLayout, ProductDetail } from './pages';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <ErrorScreen />,
+    children: [
+      {
+        index: true,
+        element: <Categories />,
+      },
+      {
+        path: 'products/:productId',
+        element: <ProductDetail />,
+      },
+    ],
+  },
+]);
+
+export default router;
