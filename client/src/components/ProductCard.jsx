9a6cf54c5128e39d20ef720e294544eb23545c7f
edriso <edriso.dev@gmail.com>
2024-05-11 12:31:54 +0300

    update ProductCard

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,35 +1,88 @@
+import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Cart } from './';
+import searchingImg from '../assets/searching.svg';
 
 class ProductCard extends Component {
   render() {
+    const { product = {} } = this.props;
     return (
       <article className="w-full px-4 sm:w-1/2 md:w-1/3 lg:w-1/4">
         <div className="p-4 overflow-hidden transition-shadow duration-500 cursor-pointer hover:shadow-xl group">
           <div className="relative mb-6">
-            <Link to="/products/123">
-              <img
-                src="https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_2_720x.jpg?v=1612816087"
-                alt="Running shoe"
-                className="object-cover w-full h-auto"
-              />
+            <Link to={`/products/${product.id}`}>
+              <div style={{ position: 'relative' }}>
+                <img
+                  src={product.gallery[0] ?? searchingImg}
+                  alt={product.name}
+                  loading="lazy"
+                  className="object-cover w-full h-auto"
+                />
+                {!product.inStock && (
+                  <div className="absolute inset-0 flex items-center justify-center px-2 py-1 text-2xl uppercase bg-white bg-opacity-50 text-muted">
+                    Out of Stock
+                  </div>
+                )}
+              </div>
             </Link>
 
-            <button
-              onClick={() => console.log('Add to cart')}
-              className="absolute bottom-0 p-2 transition-opacity duration-300 transform translate-y-1/2 rounded-full aaopacity-0 cta aagroup-hover:opacity-100 right-4"
-            >
-              <Cart color="white" className="w-5 h-5" />
-            </button>
+            {product.inStock && (
+              <button
+                onClick={() => {
+                  // add a product with its default (first in each options array) options to cart
+                  console.log('Add to cart');
+                }}
+                className="absolute bottom-0 p-2 transition-opacity duration-300 transform translate-y-1/2 rounded-full opacity-0 cta group-hover:opacity-100 right-4"
+              >
+                <Cart color="white" className="w-5 h-5" />
+              </button>
+            )}
           </div>
 
-          <h3 className="text-lg font-light capitalize">Running shoe</h3>
-          <div className="text-lg">$50.00</div>
+          <h3 className="text-lg font-light capitalize">{product.name}</h3>
+          <div className={`${!product.inStock ? 'text-muted ' : ''}text-lg`}>
+            {product.prices[0]?.currency?.symbol} {product.prices[0]?.amount}
+          </div>
         </div>
       </article>
     );
   }
 }
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    inStock: PropTypes.bool.isRequired,
+    gallery: PropTypes.arrayOf(PropTypes.string),
+    description: PropTypes.string.isRequired,
+    brand: PropTypes.string.isRequired,
+    prices: PropTypes.arrayOf(
+      PropTypes.shape({
+        amount: PropTypes.number.isRequired,
+        currency: PropTypes.shape({
+          label: PropTypes.string.isRequired,
+          symbol: PropTypes.string.isRequired,
+        }).isRequired,
+      })
+    ).isRequired,
+    category: PropTypes.string.isRequired,
+    attributes: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        items: PropTypes.arrayOf(
+          PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            value: PropTypes.string.isRequired,
+            displayValue: PropTypes.string.isRequired,
+          })
+        ).isRequired,
+      })
+    ).isRequired,
+  }).isRequired,
+};
 
 export default ProductCard;
