5833dc64bb372c42f02153a204f944077a4bd925
edriso <edriso.dev@gmail.com>
2024-05-18 14:35:55 +0300

    display currency symbol

--- client/src/components/ProductAttributes.jsx
+++ client/src/components/ProductAttributes.jsx
@@ -17,7 +17,9 @@ const ProductAttributes = ({
 
   const totalPrice =
     product.prices && product.prices.length > 0
-      ? parseFloat(product.prices[0]?.amount) * (product.quantity ?? 1)
+      ? `${product.prices[0].currency.symbol}${(
+          parseFloat(product.prices[0]?.amount) * (product.quantity ?? 1)
+        ).toFixed(2)}`
       : null;
 
   const handleAttributeClick = (attributeSetId, attribute) => {
--- client/src/components/cart/CartModal.jsx
+++ client/src/components/cart/CartModal.jsx
@@ -1,54 +1,60 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import CartModalItem from './CartModalItem';
+import { useDataContext } from '../../DataContext';
 
-class CartModal extends Component {
-  render() {
-    const { cartItems = [] } = this.props;
+function CartModal({ cartItems = [] }) {
+  const { placeOrder } = useDataContext();
 
-    const totalPrice = cartItems.reduce(
+  const totalPrice = cartItems
+    .reduce(
       (total, item) =>
         total + parseFloat(item.product?.prices[0]?.amount) * item.quantity,
       0
-    );
+    )
+    .toFixed(2);
 
-    const totalItems = cartItems.reduce(
-      (total, item) => total + item.quantity,
-      0
-    );
+  const totalItems = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
 
-    return (
-      <section className="absolute z-50 bg-white shadow-lg -right-3.5 top-full w-80 py-6 px-4 max-h-[calc(100vh-4rem)] overflow-y-auto">
-        <h2 className="mb-6">
-          <span className="font-bold">My Bag</span>
-          {!!totalItems && `, ${totalItems} item${totalItems === 1 ? '' : 's'}`}
-        </h2>
+  return (
+    <section className="absolute z-50 bg-white shadow-lg -right-3.5 top-full w-80 py-6 px-4 max-h-[calc(100vh-4rem)] overflow-y-auto">
+      <h2 className="mb-6">
+        <span className="font-bold">My Bag</span>
+        {!!totalItems && `, ${totalItems} item${totalItems === 1 ? '' : 's'}`}
+      </h2>
 
-        {totalItems === 0 ? (
-          <p className="mt-2 text-gray-500">Your bag is empty.</p>
-        ) : (
-          <>
-            <div className="space-y-8 overflow-y-auto max-h-80">
-              {cartItems.map((item) => (
-                <CartModalItem key={item.id} item={item} />
-              ))}
-            </div>
+      {totalItems === 0 ? (
+        <p className="mt-2 text-gray-500">Your bag is empty.</p>
+      ) : (
+        <>
+          <div className="space-y-8 overflow-y-auto max-h-80">
+            {cartItems.map((item) => (
+              <CartModalItem key={item.id} item={item} />
+            ))}
+          </div>
 
-            <div className="pt-4 mt-4">
-              <div className="flex items-center justify-between">
-                <h3 className="font-semibold font-roboto">Total</h3>
-                <div className="font-bold">{totalPrice.toFixed(2)}</div>
+          <div className="pt-4 mt-4">
+            <div className="flex items-center justify-between">
+              <h3 className="font-semibold font-roboto">Total</h3>
+              <div className="font-bold">
+                {`${cartItems[0]?.product.prices[0].currency.symbol}${totalPrice}`}
               </div>
-
-              <button type="button" className="w-full mt-8 btn-cta">
-                Place Order
-              </button>
             </div>
-          </>
-        )}
-      </section>
-    );
-  }
+
+            <button
+              type="button"
+              className="w-full mt-8 btn-cta"
+              onClick={placeOrder}
+            >
+              Place Order
+            </button>
+          </div>
+        </>
+      )}
+    </section>
+  );
 }
 
 CartModal.propTypes = {
